fbf675acdbb243fe6cbe907b6f4b2b634d23b8ba
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-07-05 00:54:20 +0000

    Add advanced script for log parsing

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,16 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/[A-Z]/).join
+def extract_message_info(log)
+  regex = /\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/
+  match = log.match(regex)
+  if match
+    sender = match[1]
+    receiver = match[2]
+    flags = match[3]
+    puts "#{sender},#{receiver},#{flags}"
+  end
+end
+
+log_file = ARGV[0]
+extract_message_info(log_file)
+
+#File.foreach(log_file) do |line|
