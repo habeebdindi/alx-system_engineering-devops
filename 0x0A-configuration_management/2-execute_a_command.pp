94352af21e0d56e87e0d7faf12397687fc31fc91
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-08-25 17:14:46 -0700

    Add puppet manifests

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,7 @@
+#kills a process
+exec {'kill_killmenow_proc':
+  command     => 'pkill killmenow',
+  path        => '/usr/bin:/usr/sbin:/bin:/sbin',
+  onlyif      => 'pgrep killmenow',
+  refreshonly => true,
+}
